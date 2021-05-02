24a1978d73e6d7bb1f1dad92094f21b6df478ebc
Markus Ahlstrand <markus@ahlstrand.es>
2021-05-02 21:12:03 +0200

    Expose origin middleware

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const HttpLogger = require("./services/http-logger");
-const KinesisLogger = require("./services/kinesis-logger");
-const logger = require("./middlewares/logger");
-const httpsRedirect = require("./middlewares/https-redirect");
-const rateLimit = require("./middlewares/rate-limit");
+const HttpLogger = require('./services/http-logger');
+const KinesisLogger = require('./services/kinesis-logger');
+const httpsRedirect = require('./middlewares/https-redirect');
+const logger = require('./middlewares/logger');
+const origin = require('./middlewares/origin');
+const rateLimit = require('./middlewares/rate-limit');
 
 module.exports = {
   middlewares: {
-    logger,
     httpsRedirect,
+    logger,
+    origin,
     rateLimit,
   },
   services: {
